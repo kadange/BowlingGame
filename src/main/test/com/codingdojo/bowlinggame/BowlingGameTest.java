8190956780f8949eee936e02e521237c217e2582
kadange <gskadano@gmail.com>
2018-09-23 20:57:00 +0800

    Rename test class

--- src/main/test/com/codingdojo/bowlinggame/BowlingGameTest.java
+++ src/main/test/com/codingdojo/bowlinggame/BowlingGameTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class BowlingMainTest {
+public class BowlingGameTest {
 
     private BowlingGame bowlingGame;
 
