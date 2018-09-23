75dd9a09fe5500dc844e1e77afe89819cb55feeb
kadange <gskadano@gmail.com>
2018-09-23 16:29:10 +0800

    create constant and replace field cursor to method variable

--- src/main/java/com/codingdojo/bowlinggame/BowlingGame.java
+++ src/main/java/com/codingdojo/bowlinggame/BowlingGame.java
@@ -2,12 +2,15 @@ package com.codingdojo.bowlinggame;
 
 class BowlingGame {
 
-    public static final int FIRST_ROLL = 0;
-    public static final int SECOND_ROLL = 1;
-    public static final int TOTAL_SCORE_PER_FRAME = 10;
+    private static final int FIRST_ROLL = 0;
+    private static final int SECOND_ROLL = 1;
+    private static final int TOTAL_SCORE_PER_FRAME = 10;
+    private static final int THREE_ROLLS = 3;
+    private static final int TWO_ROLLS = 2;
+    private static final int INCREMENT_BY_TWO = 2;
+    private static final int INCREMENT_BY_ONE = 1;
     private int roll = 0;
     private int[] rolls = new int[21];
-    private int cursor;
 
     void roll(int... rolls) {
         for (int pinsDown : rolls) {
@@ -25,46 +28,47 @@ class BowlingGame {
     }
 
     private int calculateTotalScore(int score) {
-        cursor = 0;
+        int cursor = 0;
         for(int frame = 0; frame < 10; frame++){
-            if(isStrike()) {
-                score = calculateScore(score, 3);
-                cursorOnNextFrame(1);
-            }else if(isSpare()) {
-                score = calculateScore(score, 3);
-                cursorOnNextFrame(2);
+            if(isStrike(cursor)) {
+                score = calculateScore(cursor, score, THREE_ROLLS);
+                cursor = cursorOnNextFrame(cursor, INCREMENT_BY_ONE);
+            }else if(isSpare(cursor)) {
+                score = calculateScore(cursor, score, THREE_ROLLS);
+                cursor = cursorOnNextFrame(cursor, INCREMENT_BY_TWO);
             }else{
-                score = calculateScore(score, 2);
-                cursorOnNextFrame(2);
+                score = calculateScore(cursor, score, TWO_ROLLS);
+                cursor = cursorOnNextFrame(cursor, INCREMENT_BY_TWO);
             }
         }
         return score;
     }
 
-    private int calculateScore(int score, int loop) {
-        for(int index = 0; index < loop; index++){
-            score += scorePerRoll(index);
+    private int calculateScore(int cursor, int score, int maxRolls) {
+        for(int index = 0; index < maxRolls; index++){
+            score += scorePerRoll(cursor, index);
         }
         return score;
     }
 
-    private void cursorOnNextFrame(int toIncrement) {
+    private int cursorOnNextFrame(int cursor, int toIncrement) {
         cursor += toIncrement;
+        return cursor;
     }
 
-    private boolean isSpare() {
-        return totalScorePerFrame() == TOTAL_SCORE_PER_FRAME;
+    private boolean isSpare(int cursor) {
+        return totalScorePerFrame(cursor) == TOTAL_SCORE_PER_FRAME;
     }
 
-    private boolean isStrike() {
-        return scorePerRoll(FIRST_ROLL) == TOTAL_SCORE_PER_FRAME;
+    private boolean isStrike(int cursor) {
+        return scorePerRoll(cursor, FIRST_ROLL) == TOTAL_SCORE_PER_FRAME;
     }
 
-    private int totalScorePerFrame() {
-        return scorePerRoll(FIRST_ROLL) + scorePerRoll(SECOND_ROLL);
+    private int totalScorePerFrame(int cursor) {
+        return scorePerRoll(cursor, FIRST_ROLL) + scorePerRoll(cursor, SECOND_ROLL);
     }
 
-    private int scorePerRoll(int rollIndex) {
+    private int scorePerRoll(int cursor, int rollIndex) {
         return rolls[cursor + rollIndex];
     }
 }
